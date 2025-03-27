ccc4b3062df03c0b255122be70a723569b1900ad
Kosiarka6000 <alankudelko2000@gmail.com>
2025-03-27 12:18:06 +0100

    Update

---
+++ C++/MWPZ06X - Nowa dzialka/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int testow;
+    int krokow;
+    cin>>testow;
+
+    for(int i=0;i<testow;i++){
+        cin>>krokow;
+        cout<<krokow*krokow<<endl;
+    }
+    return 0;
+}

822b61714c55ecb73808c07a9f6ff42aca71317b
Kosiarka6000 <alankudelko2000@gmail.com>
2025-03-28 15:59:09 +0100

    Zliczanie

--- C++/PROGC02 - Zliczanie wystąpień (kody ASCII)/main.cpp
+++ C++/PROGC02 - Zliczanie wystąpień (kody ASCII)/main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main(){
 	int kodZnaku;
 	int tabelaKodow[256]={0};
-	
-	
-	while(kodZnaku=cin.get()){
+
+
+	while((kodZnaku=getchar())!=EOF){
 		tabelaKodow[kodZnaku]++;
 	}
 	for(int i=0;i<256;i++){
